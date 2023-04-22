1e5ad51b489376b6acd9b7754d88a8b3ceefd92b
Trần Vũ Duy <107357654+Duy2763@users.noreply.github.com>
2023-04-22 18:42:24 +0700

    Hoàn thành phần thêm, xóa, sửa lên table

--- BaiTapThucHanhCuoiKi/JS/main.js
+++ BaiTapThucHanhCuoiKi/JS/main.js
@@ -10,35 +10,73 @@ var btn_addVaoBang = $('#btn-datlich');
 var modal_container = $('.modal-container');
 var tbody = $('tbody');
 var data = [];
-var stt = 0;
 
 function render() {
     var tables = '';
     for(var key in data) {
         tables += `
         <tr>
-            <td>${stt++}</td>
+            <td>${Number(key) + 1}</td>
             <td>${data[key].Ma}</td>
             <td>${data[key].MatKhau}</td>
             <td>${data[key].NgayKham}</td>
             <td>${data[key].PhuThu}</td>
             <td>${data[key].ChuyenKhoa}</td>
+            <td>
+                <button onclick="delete_item(${key})">Delete</button>
+                <button onclick="edit_item(${key})">Edit</button>
+            </td>
         </tr>
         `;
     }
-    tables.innerHTML = tables;
+    tbody.innerHTML = tables;
 }
+
+
+function delete_item(i) {
+   data.splice(i, 1);
+    render();
+}
+
+function edit_item(i) {
+    modal.classList.add('show');
+
+    var chuyenKhoaSelect = $('#txt-chuyenkhoa');
+    var chkList = $$('input[type="checkbox"]')
+    $('#txt-mabenhnhan').value = data[i].Ma;
+    $('#txt-matkhau').value = data[i].MatKhau;
+    $('#txt-date').value = data[i].NgayKham;
+    chuyenKhoaSelect.selectedIndex = 0;
+    for (var value of data[i].ListCheckbox) {
+        chkList[value].checked = true;
+    }
+}
+
+function clearInput() {
+    var chuyenKhoaSelect = $('#txt-chuyenkhoa');
+    var chkList = $$('input[type="checkbox"]')
+    $('#txt-mabenhnhan').value = "";
+    $('#txt-matkhau').value = "";
+    $('#txt-date').value = "";
+    chuyenKhoaSelect.selectedIndex = 0;
+    for (var chk of chkList) {
+        chk.checked = false;
+    }
+}
+
 
 datLich.addEventListener('click', () => {
     modal.classList.add('show')
 });
 
 close.addEventListener('click', () => {
-    modal.classList.remove('show')
+    modal.classList.remove('show');
+    clearInput();
 });
 
 btn_close.addEventListener('click', () => {
-    modal.classList.remove('show')
+    modal.classList.remove('show');
+    clearInput();
 });
 
 modal.addEventListener('click', (e) => {
@@ -48,9 +86,45 @@ modal.addEventListener('click', (e) => {
 });
 
 btn_addVaoBang.addEventListener('click', (e) => {
-   var txtMa = $('#txt-mabenhnhan').value;
-   var txtMa = $('#txt-mabenhnhan').value;
-   var txtMa = $('#txt-mabenhnhan').value;
-   var txtMa = $('#txt-mabenhnhan').value;
-   var txtMa = $('#txt-mabenhnhan').value;
+
+   var chuyenKhoaSelect = $('#txt-chuyenkhoa');
+   var chkList = $$('input[type="checkbox"]')
+
+   var ma = $('#txt-mabenhnhan').value;
+   var matKhau = $('#txt-matkhau').value;
+   var ngayKham = $('#txt-date').value;
+   var chuyenKhoa = chuyenKhoaSelect.options[chuyenKhoaSelect.selectedIndex].value;
+   var phuThu = 0;
+    for (var chk of chkList) {
+        if (chk.checked) {
+            phuThu += 500000;
+        }
+    }
+    var listCheckbox = [];
+    for (var key in chkList) {
+        if (chkList[key].checked) {
+            listCheckbox.push(key);
+        }
+    }
+    var item = {
+        Ma: ma,
+        MatKhau: matKhau,
+        NgayKham: ngayKham,
+        PhuThu: phuThu,
+        ChuyenKhoa: chuyenKhoa,
+        ListCheckbox: listCheckbox
+    }
+
+    let index = data.findIndex((a) => a.Ma == item.Ma);
+    if (index >= 0) {
+        data.splice(index, 1, item);
+    } else {
+        data.push(item);
+    }
+
+    render();
+    clearInput();
+    alert("Đặt lịch thành công")
  });
+
+ 
